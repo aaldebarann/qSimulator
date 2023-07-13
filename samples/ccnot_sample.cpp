f00ab42c9c82ca2d3fe6291329c424a274fdde12
Ivanchenko Aleksei <amivanchenko@yandex.ru>
2023-07-13 12:40:16 +0300

    just another sample

---
+++ samples/ccnot_sample.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Алексей on 13.07.2023.
+//
+
+#include <iostream>
+#include <Gates.h>
+#include "System.h"
+
+using namespace std;
+
+int main() {
+    Matrix m = CCNOT(0, 1, 2, 4);
+    for(int i = 0;  i < m.size(); i++) {
+        for(int j = 0; j < m.size(); j++) {
+            cout << m(i, j) << " ";
+        }
+        cout << endl;
+    }
+}
