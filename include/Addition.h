aa3044eab68d5b94e72b8e19a97d5248121dcc43
Ivanchenko Aleksei <amivanchenko@yandex.ru>
2023-07-14 14:23:14 +0300

    classical addition

---
+++ include/Addition.h
@@ -0,0 +1,43 @@
+//
+// Created by Алексей on 14.07.2023.
+//
+
+#ifndef QSIMULATOR_ADDITION_H
+#define QSIMULATOR_ADDITION_H
+
+#include "System.h"
+#include "Circuit.h"
+
+unsigned classicalAdd(unsigned a, unsigned b, size_t bits) {
+    if(bits > 4)
+        throw std::out_of_range("classical addition can be executed with no more than 4 bits");
+    System s(3*bits + 1);
+    // кодируем слагаемые в состоянии системы
+    Circuit init(3*bits + 1);
+    for(int i = 0; i < bits; i++) {
+        if(a % 2 == 1)
+            init.x(3*i + 1);
+        a = a >> 1;
+        if(b % 2 == 1)
+            init.x(3*i + 2);
+        b = b >> 1;
+    }
+    s.apply(init);
+    // применяем схему классического сложения
+    Circuit circ(3*bits + 1);
+    circ.add(bits);
+    s.apply(circ);
+    // измеряем состояние системы
+    unsigned res = s.measure();
+    // интерпретируем результат как сумму
+    unsigned sum = 0;
+    for(int i = 0; i < bits; i++) {
+        res = res >> 2;
+        sum += (res % 2) << i;
+        res = res >> 1;
+    }
+    sum += (res % 2) << bits;
+    return sum;
+}
+
+#endif //QSIMULATOR_ADDITION_H
--- test/test_addition.cpp
+++ test/test_addition.cpp
@@ -2,10 +2,36 @@
 // Created by Алексей on 13.07.2023.
 //
 
+#include "Addition.h"
 #include "gtest.h"
 #include "System.h"
 
 TEST(test_addition, classic_3bit_circuit) {
     System s{10};
-    Circuit circ(4)
+
+    Circuit init(10);
+    init.x(2);
+    init.x(7);
+    init.x(8);
+    s.apply(init);
+
+    Circuit circ(10);
+    circ.add(3);
+
+    s.apply(circ);
+    size_t res = s.measure();
+    std::cout << res << std::endl;
+}
+
+TEST(test_addition, classic) {
+    EXPECT_EQ(3, classicalAdd(1, 2, 2));
+    EXPECT_EQ(3, classicalAdd(0, 3, 2));
+    EXPECT_EQ(4, classicalAdd(1, 3, 2));
+    EXPECT_EQ(9, classicalAdd(4, 5, 3));
+    EXPECT_EQ(11, classicalAdd(5, 6, 3));
+    EXPECT_EQ(11, classicalAdd(4, 7, 3));
+    EXPECT_EQ(14, classicalAdd(7, 7, 3));
+    EXPECT_EQ(12, classicalAdd(4, 8, 4));
+    EXPECT_EQ(23, classicalAdd(10, 13, 4));
+    EXPECT_EQ(29, classicalAdd(14, 15, 4));
 }
