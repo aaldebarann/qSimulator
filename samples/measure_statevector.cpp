eab8188687d237e80116e44653066bd9dd723e22
aaldebarann <amivanchenko@yandex.ru>
2023-09-19 21:14:32 +0300

    new samples

---
+++ samples/measure_statevector.cpp
@@ -0,0 +1,37 @@
+//
+// Created by aleksei on 9/15/23.
+// Измерение производительности базовых гейтов (в реализации без матриц гейтов)
+#include <iostream>
+#include "StateVector.h"
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+void baseOperations(StateVector* s) {
+    // к системе применяется набор базовых гейтов
+    s->i(s->size - 2);
+    s->x(s->size - 3);
+    s->y(s->size - 4);
+    s->z(s->size - 2);
+    s->h(s->size - 3);
+    s->p(0.3 ,s->size - 4);
+    s->cnot(1, s->size - 2);
+    s->p(0.3 ,s->size - 3);
+    s->cp(0.3 ,s->size - 3, s->size - 4);
+    s->ccp(0.3 ,s->size - 2, s->size - 3, s->size - 4);
+}
+
+int main() {
+    size_t size = 27;
+    // при размере системы size=27 требуется порядка 10^8 операций
+    // ожидаемое время исполнения одного гейта: 100 ms - 1000 ms
+    // помним, что код измеряет время выполнения 10-ти базовых операций
+
+    steady_clock::time_point begin, end;
+    StateVector* s = new StateVector(size);
+    begin = steady_clock::now();
+    baseOperations(s);
+    end = steady_clock::now();
+    cout <<"total elapsed time: " << duration_cast<milliseconds>(end - begin).count() << " ms" << endl;
+    delete s;
+}
---
+++ samples/shor_sample.cpp
@@ -0,0 +1,21 @@
+//
+// Created by aleksei on 9/15/23.
+//
+#include <iostream>
+#include "shor.h"
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+void shor(int N, int bits) {
+    steady_clock::time_point begin, end;
+    begin = steady_clock::now();
+    size_t res = shor_quantum(N, bits, true);
+    end = steady_clock::now();
+    cout << res << " is divider of "<< N << endl;
+    cout <<"total elapsed time: " << duration_cast<milliseconds>(end - begin).count() << " ms" << endl;
+}
+
+int main() {
+    shor(15, 4);
+}
