8c90e5be00e07c907bd5802eb091a80eaa45fb1e
Ivanchenko Aleksei <90568101+aaldebarann@users.noreply.github.com>
2023-10-11 11:37:57 +0300

    Update shor_sample.cpp

--- samples/shor_sample.cpp
+++ samples/shor_sample.cpp
@@ -10,12 +10,12 @@ using namespace std::chrono;
 void shor(int N, int bits) {
     steady_clock::time_point begin, end;
     begin = steady_clock::now();
-    size_t res = shor_quantum(N, bits, true);
+    size_t res = shor_quantum(N, bits, false);
     end = steady_clock::now();
     cout << res << " is divider of "<< N << endl;
     cout <<"total elapsed time: " << duration_cast<milliseconds>(end - begin).count() << " ms" << endl;
 }
 
 int main() {
-    shor(15, 4);
+    shor(21, 5);
 }
